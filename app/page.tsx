3a98ae9ca259eba52c4e280f6660473bc01f2811
noussaibadha <n_dhaou@stu-digital-campus.fr>
2025-06-17 11:18:01 +0200

    ajout spot + proposition

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,41 @@
-import Image from "next/image";
+'use client'
+import { useEffect, useState } from 'react'
+import { createClient } from '@supabase/supabase-js'
 
-export default function Home() {
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
+
+export default function HomePage() {
+  const [articles, setArticles] = useState<any[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await supabase
+        .from('articles')
+        .select('*')
+        .eq('status', true)
+
+      setArticles(data || [])
+    }
+
+    fetchData()
+  }, [])
+
   return (
-    <div className="grid mt-74 grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>« Un scorpion, cherchant à traverser une rivière, demande à une grenouille de le prendre sur son dos.<br></br>
-        <br></br>
-        – « Pour qui me prends-tu, scorpion ?? Je te connais, tu vas me piquer !! »<br></br>
-        <br></br>
-        – « Mais non, grenouille ! Tu peux me faire confiance. Si je te pique, je me noierai moi aussi ! »<br></br>
-        <br></br>
-        La grenouille hésite mais finit par céder sous les insistances du scorpion. Elle le fait monter sur son dos et s’engage dans la rivière.<br></br>
-        <br></br>
-        Arrivés au milieu, le scorpion plante son dard profondément dans le dos de la grenouille.<br></br>
-        <br></br>
-        Celle-ci est paralysée et se met à couler, entraînant le scorpion avec elle. Elle parvient cependant à poser une dernière question :<br></br>
-        <br></br>
-        – « Mais enfin, scorpion ! Pourquoi as-tu fait ça ?? Nous allons mourir tous les deux !! »<br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        – <span className="text-amber-300">« C’est dans ma nature ! »</span>, répond le scorpion. »</h1>
-    </div>
-  );
+    <main className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Articles</h1>
+      <div className="grid gap-4">
+        {articles?.map((article) => (
+          <div key={article.id} className="border p-4 rounded shadow">
+            <h2 className="text-xl font-semibold">{article.title}</h2>
+            <p>{article.description}</p>
+            <p className="text-sm text-gray-500">{article.adress} - {article.categorie}</p>
+            {article.img && <img src={article.img} alt={article.title} className="w-full max-w-md mt-2" />}
+          </div>
+        ))}
+      </div>
+    </main>
+  )
 }
