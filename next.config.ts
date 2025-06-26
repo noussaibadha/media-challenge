7ef76acb3c3f46908de40fc330cd94a0e1fbcb73
drysf <drys.ferhi@gmail.com>
2025-06-26 20:30:49 +0200

    ajout conf pour build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  // Configuration Next.js moderne - serverActions n'est plus nécessaire
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default nextConfig
